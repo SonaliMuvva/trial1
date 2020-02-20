01d87dfb84df277d318c3f6e434776ea7cfa8b72
sonalimuvva <sn621173@dal.ca>
2020-02-20 18:50:05 +0000

    initial module

---
+++ task1A.py
@@ -0,0 +1,27 @@
+
+def list_gc():
+
+    stream = open('Trails.csv', 'r')
+
+    line = stream.read().split(',')
+
+    print(line)
+
+    list1= []
+
+    list2 = []
+
+    for each in lines:
+
+        list1 = each.split()
+
+        if len(list1) ==:
+
+            print(list1)
+
+            list2.append((list1[1],"general condition ="+list1[3]))
+
+    return list2
+
+print(list_gc())
+
